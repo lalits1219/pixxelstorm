d013a8763e83564e56d3ac3711994176d23bde22
unknown <sharmalalit.1503@gmail.com>
2026-01-07 01:43:00 +0530

    Convert data to CMS structure

--- src/pages/Airdrops.jsx
+++ src/pages/Airdrops.jsx
@@ -1,12 +1,17 @@
-import data from "../data/airdrops.json"
+const files = import.meta.glob("../data/airdrops/*.json", {
+  eager: true
+})
 
 export default function Airdrops() {
+  const data = Object.values(files)
+
   return (
     <div className="container">
       <h1>Airdrops</h1>
+
       <div className="grid">
-        {data.map((item, i) => (
-          <div className="card" key={i}>
+        {data.map((item, index) => (
+          <div className="card" key={index}>
             <span className="badge">{item.status}</span>
             <h3>{item.title}</h3>
             <p>{item.description}</p>
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,12 +1,17 @@
-import data from "../data/news.json"
+const files = import.meta.glob("../data/news/*.json", {
+  eager: true
+})
 
 export default function News() {
+  const data = Object.values(files)
+
   return (
     <div className="container">
       <h1>Crypto News</h1>
+
       <div className="grid">
-        {data.map((item, i) => (
-          <div className="card" key={i}>
+        {data.map((item, index) => (
+          <div className="card" key={index}>
             <h3>{item.title}</h3>
             <p>{item.summary}</p>
           </div>
@@ -15,3 +20,4 @@ export default function News() {
     </div>
   )
 }
+
